75ae7355668784c7dae417183d8d1d93395514a1
MS Rahman <msrahman048gmail.com>
2025-01-01 18:27:23 +0600

    23-9 & 23-10 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
 
   return (
     <div>
       <h1>Counter With Redux</h1>
 
-      <button>Increment</button>
-      <div>0</div>
-      <button>Decrement</button>
-     
-  
+      <button onClick={handleIncrement}>Increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
