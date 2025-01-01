6490dc3bed827eccceb28e2b84b98c55794a5e33
MS Rahman <msrahman048gmail.com>
2025-01-01 12:22:18 +0600

    23-7 completed

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Counter With Redux</h1>
+
+      <button>Increment</button>
+      <div>0</div>
+      <button>Decrement</button>
+     
+  
+    </div>
+  )
+}
+
+export default App
